1a66400b4f7ff436ab43f82b87ff235ac36e1f00
Prateek <prateek.kumar.original@gmail.com>
2023-12-07 01:32:17 +0530

    Add tailwind and create base template

--- money/expenses/models.py
+++ money/expenses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_total_price(self):
+        return self.quantity * self.price
--- money/expenses/views.py
+++ money/expenses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView, DetailView
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'expenses/index.html')
