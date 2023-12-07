dee4938e6fe6a6bc0e77c861616ccc554d2760e8
Prateek <prateek.kumar.original@gmail.com>
2023-12-07 23:59:52 +0530

    Add expense form

--- money/expenses/models.py
+++ money/expenses/models.py
@@ -3,8 +3,7 @@ from django.db import models
 # Create your models here.
 class Expense(models.Model):
     name = models.CharField(max_length=200)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField()
     category = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
 
--- money/expenses/views.py
+++ money/expenses/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView
+from .forms import ExpenseForm
+from .models import Expense
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'expenses/index.html')
+        expense_form = ExpenseForm()
+        expenses = Expense.objects.all()
+        return render(request, 'expenses/index.html', {'expense_form': expense_form, 'expenses': expenses})
+    
+    def post(self, request):
+        expense_form = ExpenseForm(request.POST)
+        if expense_form.is_valid():
+            expense_form.save()
+            return render(request, 'expenses/index.html', {'expense_form': ExpenseForm()})
+        return render(request, 'expenses/index.html', {'expense_form': expense_form})
